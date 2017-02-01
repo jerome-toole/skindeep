51a610b84d372c81fbd607f868ba19900eb08d1e
Sam Briggs <wasso14@hotmail.com>
2017-02-01 01:51:36 +0000

    [iss11] BUGFIX: Reinstated break to skindeep.js

--- catch-base-pro/js/skindeep.js
+++ catch-base-pro/js/skindeep.js
@@ -7,6 +7,7 @@ jQuery(document).ready(function($) {
 
   var postgrid = (function( $container ) {
     var $container = jQuery($container);
+    $container.imagesLoaded( function(){ 
     
       $container.masonry({ 
         itemSelector: '.post', 
