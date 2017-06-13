dca4824058ca11625caf8ad4a768f96e94f6073f
Sam Briggs <wasso14@hotmail.com>
2017-06-14 00:40:07 +0100

    Add shell script for installing plugins with wpcli

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+wp plugin install https://github.com/afragen/github-updater/archive/master.zip --activate
+wp plugin install https://github.com/wp-sync-db/wp-sync-db/archive/master.zip --activate
+wp plugin install https://github.com/wp-sync-db/wp-sync-db-media-files/archive/master.zip --activate
+wp plugin install advanced-custom-fields --version=4.4.11 --activate
+wp plugin install catch-ids --version=1.4.5 --activate
+wp plugin install enhanced-category-pages --version=2.3.0 --activate
+wp plugin install icegram --version=1.10.4 --activate
+wp plugin install ithemes-exchange --version=1.36.4 --activate
+wp plugin install category-related-products-for-ithemes-exchange --version=1.0.2 --activate
+wp plugin install easy-google-fonts --version=1.3.9 --activate
+wp plugin install ticket-tailor --version=1.5 --activate
+wp plugin install wordpress-seo --version=3.0.7 --activate
+wp plugin install shortcode-widget --version=1.3 --activate
+wp plugin install image-widget --version=4.4.3 --activate
+wp plugin install https://github.com/briggySmalls/ithemes-exchange-digital-variants/archive/master.zip --activate
+wp plugin install social-share-buttons-by-supsystic --version=1.5.9 --activate
+wp plugin install enable-media-replace --version=3.0.4 --activate
+wp plugin install better-analytics --version=1.2.3 --activate
+wp plugin install mailchimp --version=1.5.7 --activate
+wp plugin install fd-footnotes --version=1.36 --activate
+wp plugin install media-file-sizes --version=1.8
+wp plugin install enhanced-media-library --version=2.1.7
